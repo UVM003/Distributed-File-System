707a0df25968221989611e6e917e93702e6fd874
venkatachalaMelady <venkatachala.melady@zysk.tech>
2025-04-12 17:29:57 +0530

    feat: Completed the p2p transport

--- main.go
+++ main.go
@@ -1,18 +1,37 @@
 package main
 
 import (
-	
+	"fmt"
 	"foreverstore/p2p"
 	"log"
 )
 
+func OnPeer (peer p2p.Peer) error { 
+
+	//case 1
+	// fmt.Println("doing some logic with the peer outside of TCPTransport")
+	
+
+	peer.Close()
+	return nil
+}
+
 func main(){
 	tcpOpts:=p2p.TCPTransportOpts{
 		ListenAddr: ":3000",
-		Decoder: p2p.GOBDecoder{} ,
+		Decoder: p2p.DefaultDecoder{} ,
 		HandshakeFunc:p2p.NOPHandshakeFunc,
+		OnPeer: OnPeer,
 	}
 	tr:=p2p.NewTCPTransport(tcpOpts)
+
+	go func(){
+		for {
+			msg := <-tr.Consume()
+			// fmt.Println("Its from main")
+			fmt.Printf("%+v\n",msg)
+		}
+	}()
 
 	if err:=tr.ListenAndAccept(); err!=nil{
 		log.Fatal(err)
